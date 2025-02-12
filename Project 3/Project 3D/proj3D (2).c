37617bdc97042a790d087edcce809652824bc240
Alexia Crawford <147873855+alexiacrawford@users.noreply.github.com>
2025-02-12 14:06:27 -0800

    Add files via upload

---
+++ Project 3/Project 3D/proj3D (2).c
@@ -0,0 +1,37 @@
+/* Don't forget to compile with -O2 */
+
+/* Array-Based Dictionary
+ *   Big O Store Time: O(1)
+ *   Actual Store Time: (put your runtime here, in seconds)
+ *   Do you believe this result makes sense?   (Y/N)
+ *
+ *   Big O Fetch Time: O(N)
+ *   Actual Fetch Time: (put your runtime here, in seconds)
+ *   Do you believe this result makes sense?   (Y/N)
+ */
+
+/* Linked List-Based Dictionary
+ *   Big O Store Time: 
+ *   Actual Store Time: (put your runtime here, in seconds)
+ *   Do you believe this result makes sense?   (Y/N)
+ *
+ *   Big O Fetch Time: 
+ *   Actual Fetch Time: (put your runtime here, in seconds)
+ *   Do you believe this result makes sense?   (Y/N)
+ */
+
+/* Hash Table-Based Dictionary
+ *   Big O Store Time: 
+ *   Actual Store Time: (put your runtime here, in seconds)
+ *   Do you believe this result makes sense?   (Y/N)
+ *
+ *   Big O Fetch Time: 
+ *   Actual Fetch Time: (put your runtime here, in seconds)
+ *   Do you believe this result makes sense?   (Y/N)
+ */
+
+/* Additional studies:
+ *    Leave this blank if you did no additional studies.
+ */
+
+
